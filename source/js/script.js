34076b2055c76b53642b8869c1fc3e8e01fef32a
Oxana Garascenco <oxana.garashcenco@gmail.com>
2018-05-11 15:33:49 +0300

    открытие модального окна по клику на корзинку в каталоге - не работает

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,15 @@
 var mainModal = document.querySelector(".main-modal");
 var bestItemOrder = document.querySelector(".best-item__order");
+var goodsItemOrder = document.querySelector(".goods-item__order-item");
 var orderModalButton = document.querySelector(".order-modal__button");
 
+
 bestItemOrder.addEventListener("click", function(e) {
+  e.preventDefault();
+  mainModal.classList.add("js-modal-show");
+});
+
+goodsItemOrder.addEventListener("click", function(e) {
   e.preventDefault();
   mainModal.classList.add("js-modal-show");
 });
